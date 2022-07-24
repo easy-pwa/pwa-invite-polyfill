cff3be4d4e075d948de101454735b11aad1c8058
Adrien Peyre <dev@adrien.peyre.fr>
2022-07-24 15:06:17 +0000

    init

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  parser:  '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'airbnb-base',
+    "plugin:@typescript-eslint/eslint-recommended",
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    'import/extensions': ['error', {
+      'ts': 'never',
+      'json': 'always'
+    }],
+    'class-methods-use-this': 'off',
+    'no-restricted-syntax': 'off',
+    'no-param-reassign': 'off',
+    'no-cond-assign': 'off',
+    'no-useless-escape': 'off',
+    "no-useless-constructor": "off",
+    "@typescript-eslint/no-useless-constructor": ["error"],
+    'quotes': 'error',
+    'semi': 'error',
+    'comma-dangle': ['error', {
+      'arrays': 'always',
+    }],
+    'arrow-parens': ['error', 'as-needed'],
+    'max-len': 'off',
+  },
+  env: {
+    browser: true,
+  },
+  settings: {
+    'import/resolver': 'webpack',
+  },
+  parserOptions:  {
+    ecmaVersion:  2018,
+    sourceType:  'module',
+  },
+};
---
+++ src/Helper/Render/HelperRenderer.ts
@@ -0,0 +1,29 @@
+export default class HelperRenderer {
+  public createHelperPopup(htmlTemplate: string): void {
+    const popupContent = document.createElement('div');
+    popupContent.classList.add('pwa-homescreen-helper');
+    popupContent.innerHTML = `
+            <h1>Installer l\'application</h1>
+            <div class="message">${htmlTemplate}</div>
+            <div class="button">C'est fait !</div>
+        `;
+
+    const mask = document.createElement('div');
+    mask.classList.add('pwa-homescreen-helper-mask');
+
+    const closeHelper = (): void => {
+      document.body.classList.remove('pwa-helper-active');
+
+            document.getElementsByClassName('pwa-homescreen-helper')[0]?.remove();
+            document.getElementsByClassName('pwa-homescreen-helper-mask')[0]?.remove();
+    };
+
+    popupContent.addEventListener('click', closeHelper);
+    // window.setTimeout(closeHelper, 12000);
+
+    document.body.appendChild(popupContent);
+    document.body.appendChild(mask);
+
+    document.body.classList.add('pwa-helper-active');
+  }
+}
